df9c680305a67174a5b87ec86971ea3f53b8dda1
Arrnv <119531646+Arrnv@users.noreply.github.com>
2025-07-09 17:54:16 +0530

    push 7

--- server.js
+++ server.js
@@ -52,8 +52,7 @@ app.use('/api/amenities', amenitiesRouter);
 
 
 app.use(errorHandler);
-const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
