0ab4d04b54d75d64e07ce4912ed1bcd0884b2a8d
Arrnv <119531646+Arrnv@users.noreply.github.com>
2025-10-15 13:41:23 +0530

    apple

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
   'https://desi22.com',
   'https://www.desi22.com',
   "http://localhost:3000",
-  "http://localhost:8081"
+  "http://localhost:8081",
+  "https://appleid.apple.com"
 ];
 
 const corsOptions = {
